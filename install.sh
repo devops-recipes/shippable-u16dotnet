a63b6bee15d8e0b27650cba757be43dd16e5cd7e
ambarish2012 <achitnis2012@gmail.com>
2017-12-05 12:45:37 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@
 # Fix temporary directory permissions
 chmod 1777 /tmp
 
-# Install (beta) Mono package key
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu beta-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-beta.list
-apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+sudo apt-get update
 
 # Install (beta) Mono development tools
 apt-get install -y mono-devel
