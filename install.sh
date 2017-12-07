edcb446622085b3363d18416783f146e3148c683
ambarish2012 <achitnis2012@gmail.com>
2017-12-06 18:51:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -e
 
-# Fix temporary directory permissions
-chmod 1777 /tmp
-
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 sudo apt-get update
 
-# Install (beta) Mono development tools
+# Install Mono development tools
 apt-get install -y mono-devel
+
+# Install .NET Core 2.0 SDK and runtime
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.0.0
 
 # Install zip
 apt-get install -y zip
