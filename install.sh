c7064a4cb24a80d152f76da6a9d83559b3ef2a06
ambarish2012 <achitnis2012@gmail.com>
2017-12-06 19:26:51 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ apt-get install -y mono-devel
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.0.0
